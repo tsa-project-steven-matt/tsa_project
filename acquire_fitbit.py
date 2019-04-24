f7dd389d91343a20ea3714f4130514e9b6b27768
Matthew Capper <matthew.capper01@gmail.com>
2019-04-24 09:23:24 -0500

    4_24 920AM

---
+++ acquire_fitbit.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import numpy as np
+
+# Making each csv a DataFrame that is split on new line
+
+# assigning each csv to a variable
+a = 'fitbit/2018-04-26_through_2018-05-26.csv'
+b = 'fitbit/2018-05-27_through_2018-06-26.csv'
+c = 'fitbit/2018-06-27_through_2018-07-27.csv'
+d = 'fitbit/2018-07-28_through_2018-08-26.csv'
+e = 'fitbit/2018-08-27_through_2018-09-26.csv'
+g = 'fitbit/2018-09-27_through_2018-10-27.csv'
+h = 'fitbit/2018-10-28_through-2018-11-27.csv'
+i = 'fitbit/2018-11-28_through_2018-12-28.csv'
+# file_list = [a,b,c,d,e,g,h,i]
+
+# for letter in file_list:
+#     FILE = letter
+#     with open(FILE) as f:
+#         contents = f.read()
+#     letter = contents.split('\n')
+#     letter = pd.DataFrame(letter)
+
+# for letter in file_list:
+#     letter = letter.iloc[35:67]
+
+FILE = a
+with open(FILE) as f:
+    contents = f.read()
+a = contents.split('\n')
+a = pd.DataFrame(a)
+
+FILE = b
+with open(FILE) as f:
+    contents = f.read()
+b = contents.split('\n')
+b = pd.DataFrame(b)
+
+FILE = c
+with open(FILE) as f:
+    contents = f.read()
+c = contents.split('\n')
+c = pd.DataFrame(c)
+
+FILE = d
+with open(FILE) as f:
+    contents = f.read()
+d = contents.split('\n')
+d = pd.DataFrame(d)
+
+FILE = e
+with open(FILE) as f:
+    contents = f.read()
+e = contents.split('\n')
+e = pd.DataFrame(e)
+
+FILE = g
+with open(FILE) as f:
+    contents = f.read()
+g = contents.split('\n')
+g = pd.DataFrame(g)
+
+FILE = h
+with open(FILE) as f:
+    contents = f.read()
+h = contents.split('\n')
+h = pd.DataFrame(h)
+
+FILE = i
+with open(FILE) as f:
+    contents = f.read()
+i = contents.split('\n')
+i = pd.DataFrame(i)
+
+# just activities: 
+a = a.iloc[35:67]
+b = b.iloc[36:67]
+c = c.iloc[36:67]
+d = d.iloc[35:65]
+e = e.iloc[36:67]
+g = g.iloc[36:67]
+h = h.iloc[36:67]
+i = i.iloc[36:45]
+
+def combine_logs():
+    df = a.append(b).append(c).append(d).append(e).append(g).append(h).append(i)
+    df = df.reset_index()
+    return df
+
+
+def split_cols(df):
+    df = df[0].str.split('","', expand=True)
+    return df
+
+def rename_cols(df):
+    df.rename(columns={0: 'date', 
+                   1: 'calories_burned', 
+                   2: 'steps', 
+                   3: 'distance',
+                   4: 'floors',
+                   5: 'minutes_sedentary',
+                   6: 'minutes_lightly_active',
+                   7: 'minutes_fairly_active',
+                   8: 'minutes_very_active',
+                   9: 'activity_calories'}, inplace=True)
+    df.drop(index=0, inplace=True)
+    return df
+
+def get_fitbit():
+    df = combine_logs()
+    df = split_cols(df)
+    df = rename_cols(df)
+    return df
